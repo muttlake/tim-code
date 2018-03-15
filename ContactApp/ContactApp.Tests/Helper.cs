bd88189dbb6710a998f55983c0ae05831b4e959c
timothy shepard <shepbop@gmail.com>
2018-03-15 15:19:18 -0400

    Added Helper Unit Tests

--- ContactApp/ContactApp.Tests/Helper.cs
+++ ContactApp/ContactApp.Tests/Helper.cs
@@ -1,20 +1,59 @@
-
+using Xunit;
+using ContactApp.Library.Models;
+using ContactApp.Library.Enums;
+using ContactApp.Library;
+using System.Collections.Generic;
 
 namespace ContactApp.Tests
 {
     public class Helper
     {
+
+        //Need Attributes (method attributes), do not exist in C++
+        [Fact] // Allows XUnit to figure out special methods it needs to run
         public void Test_ContactHelper_Add()
         {
             //arrange
             var ch = new ContactHelper<Person>();
-            var expected = 1;
 
             //act
-            ch.Add(new Person());
+            var added = ch.Add(new Person());
+            var actual = ch.Read();  //Testing Read and Add technically
 
             //Assert
-            Assert.IsTrue(expected == ch.Size());
+            Assert.True(added);
         }
+
+        [Fact]
+        public void Test_ContactHelper_Add_Negative()
+        {
+            //arrange
+            var ch = new ContactHelper<Person>();
+
+            //act
+            var added = ch.Add(new Person());
+            var actual = ch.Read();  //Testing Read and Add technically
+
+            //Assert
+            Assert.False(!added);
+        }
+
+        [Fact]
+        public void Test_ContactHelper_Read()
+        {
+            //arrange
+            var ch = new ContactHelper<Person>();
+            var expected = 0;
+
+            //act
+            var actual = ch.Read();
+
+            var newList = new List<Person>();
+            //assert
+            Assert.True(actual.GetType() == typeof(List<Person>));
+            Assert.NotNull(actual);
+            Assert.True(actual.Count >= expected);
+        }
+
     }
 }
