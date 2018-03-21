31c65cfdb3123c970cbea1ff600d0ed87e3d5485
timothy shepard <shepbop@gmail.com>
2018-03-21 09:09:52 -0400

    Made more queries in dml.sql

--- SqlServer/dml.sql
+++ SqlServer/dml.sql
@@ -45,7 +45,8 @@ WHERE FirstName = 'john' or MiddleName = 'john' or LastName = 'john';
 
 SELECT FirstName, MiddleName, LastName
 FROM Person.Person
-WHERE FirstName = 'john' or MiddleName = 'john' or LastName = 'john';
+WHERE FirstName = 'john' or MiddleName = 'john' or LastName = 'john'
+COLLATE Latin1_General_CS_AS_KS_WS; -- Not working, still shows 'John'
 
 -- How to find anybody that has 'john' in their name
 SELECT FirstName, MiddleName, LastName
@@ -67,6 +68,76 @@ WHERE FirstName LIKE 'j___';
 SELECT FirstName, MiddleName, LastName
 FROM Person.Person
 WHERE FirstName LIKE 'j[a-k][a-k][a-k]';
+
+SELECT Count(FirstName), FirstName -- outputs a count, firstname for each group
+FROM Person.Person
+WHERE LastName = 'Smith'
+GROUP BY FirstName;  -- Rolls up each unique FirstName into a Group
+
+
+SELECT Count(FirstName), FirstName, MiddleName
+FROM Person.Person
+WHERE LastName = 'Smith'
+GROUP BY FirstName, MiddleName;  -- Groups Unique FirstName, MiddleName pairs
+
+
+-- Only get groups with counts > 1
+SELECT Count(*), FirstName, MiddleName
+FROM Person.Person
+WHERE LastName = 'Smith'
+GROUP BY FirstName, MiddleName
+HAVING Count(*) > 1;
+
+-- Only get groups with counts = 1 order by MiddleName Ascending, FirstName Descending
+-- This is a complete select statement
+SELECT Count(*), FirstName, MiddleName as MN
+FROM Person.Person as pp
+WHERE LastName = 'Smith'
+GROUP BY FirstName, MiddleName
+HAVING Count(*) = 1
+ORDER BY MN ASC, FirstName DESC;
+
+-- The Select statement works this way
+-- from > where > group by > having > select > order by
+
+
+-- Select all persons with no middle name
+SELECT * 
+FROM Person.Person
+WHERE MiddleName = '';
+
+
+-- select all persons with modified date in the latest year.
+SELECT FirstName, LastName, ModifiedDate
+FROM Person.Person
+WHERE ModifiedDate >= '2015-01-01';
+
+
+SELECT FirstName, LastName, ModifiedDate
+FROM Person.Person;
+
+-- select the full name of each person like "fred t. belotte"
+SELECT Name = FirstName + ' ' + MiddleName + ' ' + LastName
+FROM Person.Person;
+-- This is technically an error because there are so many NULL
+-- WHERE MiddleName is Not NULL AND FirstName is Not NULL and LastName is Not NULL;
+
+-- the 3 most popular first names
+SELECT TOP 3 Count(*), FirstName
+FROM Person.Person as pp
+GROUP BY FirstName
+ORDER BY Count(*) DESC;
+
+-- the 3 least popular last names
+SELECT TOP 3 Count(*), LastName
+FROM Person.Person as pp
+GROUP BY LastName
+ORDER BY Count(*) ASC;
+
+SELECT Count(*), LastName
+FROM Person.Person as pp
+GROUP BY LastName
+ORDER BY Count(*) ASC;
 
 -- INSERT
 
