fd38cda3eb5e1bcdbb8dcf3b85ba68b559fdbc03
timothy shepard <shepbop@gmail.com>
2018-04-11 12:42:46 -0400

    Added DotnetBogData to WebAPI project

---
+++ DotnetBlogData/DotnetBlogData.DB/BlogDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotnetBlogData.DB
+{
+    public class BlogDBContext : DbContext
+    {
+        public BlogDBContext(DbContextOptionsBuilder options, IConfiguration configuration)
+        {
+            //options.UseSqlServer();
+        }
+    }
+}
