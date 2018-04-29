ec86d22cae0831a351926cc96ccb121cae7dd5e0
timothy shepard <shepbop@gmail.com>
2018-04-28 20:41:53 -0400

    Made Message component

--- Heroes2/angular-tour-of-heroes/src/app/app.module.ts
+++ Heroes2/angular-tour-of-heroes/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroService } from './hero.service';
+import { MessagesComponent } from './messages/messages.component';
 
 
 
@@ -14,7 +15,8 @@ import { HeroService } from './hero.service';
   declarations: [
     AppComponent,
     HeroesComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
