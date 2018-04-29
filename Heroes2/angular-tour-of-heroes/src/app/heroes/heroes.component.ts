86550ccd7bec195808f5a5558f1acb8903409157
timothy shepard <shepbop@gmail.com>
2018-04-29 13:52:47 -0400

    Finished ToH

--- Heroes2/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ Heroes2/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
 
@@ -8,31 +9,30 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+  heroes: Hero[];
 
-   heroes = [];
+  constructor(private heroService: HeroService) { }
 
-   selectedHero: Hero;
+  ngOnInit() {
+    this.getHeroes();
+  }
 
-   //hero: Hero = {
-   //  
-   //   id: 1,
-   //   name: 'Windstorm'
-   //
-   //};
-
-   constructor(private heroService: HeroService) { }
-
-   ngOnInit() {
-     this.getHeroes();
-   }
-
-
-   onSelect(hero: Hero): void {
-    this.selectedHero = hero;
+  getHeroes(): void {
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes);
     }
 
-    getHeroes(): void {
-       this.heroService.getHeroes()
-       .subscribe(heroes => this.heroes = heroes);
-    }
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe();
+  }
 }
